9aa83fc26025f9f7ecb292b0957fb7e9c8c57bf3
Baolo <524172671@qq.com>
2016-11-03 15:20:39 +0800

    实现登录和注册功能

--- app/js/enroll.js
+++ app/js/enroll.js
@@ -1,71 +1,128 @@
 
 
-
+;(function(){
 document.addEventListener("DOMContentLoaded",function(){
 	
 	var oinput=document.querySelectorAll("input");
 	var btn=document.querySelector("button");
 	
-	
-	
-	console.log(oinput.length);
-	
-	
 	var wordList = localStorage.getItem('wordList');
 		wordList = wordList ? JSON.parse(wordList) : [];
 	
-	
+		//点击通过验证；
 		btn.onclick = function(){
+			regData();
+		}
+	//封装函数本地存储
+	function regData(){
 			var obj = {};
-			obj.en = oinput[0].value;
-			obj.cn = oinput[2].value;
-		
-
+			obj.name = oinput[0].value;
+			obj.psW = oinput[2].value;
 			// 把当前单词写入数组
 			wordList.unshift(obj);
-
 			// 保存到本地存储
-			localStorage.setItem('wordList',JSON.stringify(wordList));
-			
+			console.log(flag1);
+			if(flag1 == true){
+				if(flag2 == true){
+					if(flag3 == true){
+						if(flag4 == true){
+							localStorage.setItem('wordList',JSON.stringify(wordList));
+							alert("地球人，您已经越过太阳系，快去登录吧！");
+							setTimeout(window.open("register.html"),1000);
+						}
+					}
+				}
+			}
+	}
+	//设置全局的变量标记；
+	var flag1 = false;//标记用户；
+	var flag2 = false;//标记密码；
+	var flag3 = false;//标记重写密码；
+	var flag4 = false;//标记推荐人帐号；
+	//用户名验证；
+	oinput[0].onblur = function(){
+		registernNameTake();
+	}
+	//密码验证；
+	oinput[2].onblur = function(){
+		registerPswTake();
+	}
+		
+	//确认密码验证；
+	oinput[3].onblur = function(){
+		registerPswAgTake();
+	}
+	//推荐人验证；
+	oinput[4].onblur = function(){
+		registerTjrTake();
+	}
+//账户手机号码验证
+function registernNameTake() {
+		var _this = oinput[0];
+		var name = oinput[0].value;
+		
+		if(name == ''){
+			_this.style.borderColor = "#f20266";
+			return true;
+		}else if(/^[1][358][0-9]{9}$/.test(name)){
+			_this.style.borderColor = "#ccc";
+			flag1 = true;
+			return false;
 		}
-	
-	
-//		oinput[0].onblur = function() {
-//		var pa = this.value;
-//		var ss = /^1\d{10}$/.test(pa);
-//		if(ss) {
-//			this.classList.add("has-success");
-//                    console.log(1111);
-//		} else {
-//			this.classList.add("has-error");
-//		}
-//
-//	}
-//	
-//	
-//		oinput[2].onblur = function() {
-//		var pass = this.value;
-//		var ss = /\w{8,20}/.test(pass);
-//		if(ss) {
-//			this.classList.add("has-success");
-//		} else {
-//			
-//			this.classList.add("has-error");
-//		}
-//	}
-//
-//	oinput[3].onkeyup = function() {
-//
-//		if(oin[2].value != oin[3].value) {
-//			this.classList.add("has-success");
-//		} else {
-//			this.classList.add("has-error");
-//
-//		}
-//	}
+};
+//密码验证；
+function registerPswTake(){
+		var _this = oinput[2];
+		var name = oinput[2].value;
+		var tishi = document.querySelector(".hxy-active");
+		if(name == ''){
+			_this.style.borderColor = "#f20266";
+			return true;
+		}
+		else if(/[a-z0-9A-Z]{6,20}$/.test(name)){
+			flag2 = true;
+			_this.style.borderColor = "#ccc";
+			tishi.innerHTML = "";
+			return false;
+		}else{
+			_this.style.borderColor = "#f20266";
+			tishi.style.color = "#f20266";
+			tishi.innerHTML = "请输入6到20位字母和数字的组合的密码";
+		}
+};
+//确认密码
+function registerPswAgTake(){
+		var _this = oinput[3];
+		var name = oinput[3].value;
+		var tishi = document.querySelector(".hxy-active2");
+		if(name == ""){
+			_this.style.borderColor = "#f20266";
+			return true;
+		}else if(name === oinput[2].value){
+			flag3 = true;
+			_this.style.borderColor = "#ccc";
+			tishi.innerHTML = "";
+		}else{
+			_this.style.borderColor = "#f20266";
+			tishi.style.color = "#f20266";
+			tishi.innerHTML = "请正确匹配密码";
+		}
+};
+//推荐人帐号；
+function registerTjrTake(){
+			var _this = oinput[4];
+		var name = oinput[4].value;
+		
+		if(name == ''){
+			_this.style.borderColor = "#f20266";
+			return true;
+		}else if(/^[1][358][0-9]{9}$/.test(name)){
+			_this.style.borderColor = "#ccc";
+			flag4 = true;
+			return false;
+		}
+}
 
 
-	
-	
-	
 })
+})();
